2add6cdbafdf3e11e61abf5472410ba729b00c27
gvadivelu <49655680+gvadivelu@users.noreply.github.com>
2019-05-21 22:32:11 -0700

    diagonalDifference
    PRACTICE
    COMPETE
    JOBSNEW
    LEADERBOARD
    
    gayathri_vadive1
    PracticeAlgorithmsWarmupDiagonal Difference
    Diagonal Difference
    
    39 more points to get your next star!
    Rank: 800101|Points: 61/100
    Problem Solving
    Problem
    Submissions
    Leaderboard
    Discussions
    Editorial
    Given a square matrix, calculate the absolute difference between the sums of its diagonals.
    For example, the square matrix  is shown below:
    1 2 3
    4 5 6
    9 8 9  
    The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
    Function description
    Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
    diagonalDifference takes the following parameter:
    arr: an array of integers .
    Input Format
    The first line contains a single integer, , the number of rows and columns in the matrix . 
    Each of the next  lines describes a row, , and consists of  space-separated integers .
    Constraints
    
    Output Format
    Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
    Sample Input
    3
    11 2 4
    4 5 6
    10 8 -12
    Sample Output
    15
    Explanation
    The primary diagonal is:
    11
       5
         -12
    Sum across the primary diagonal: 11 + 5 - 12 = 4
    The secondary diagonal is:
         4
       5
    10
    Sum across the secondary diagonal: 4 + 5 + 10 = 19 
    Difference: |4 - 19| = 15

---
+++ diagonalDifference.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+        
+        int size = arr.length;
+        int d1_sum =0,d2_sum =0;
+        for(int i =0; i< size;i++)
+        {
+            d1_sum += arr[i][i];
+            d2_sum += arr[i][size-i-1];
+        }
+        
+        return Math.abs(d1_sum - d2_sum);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
